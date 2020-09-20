e18e24a7041c8650440155eb2c30e89b8cd74194
haykel grioui <haykel.grioui.pro@hotmail.com>
2020-09-20 15:32:48 +0200

    x

--- Jenkinsfile copy.groovy
+++ Jenkinsfile copy.groovy
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat
-class Constants {
+// Constants Region
+class PiplineParameters {
   static final String Project = 'Project'
   static final List ProjectTypeList = ['Batch', 'Service', 'WebSite']
   static final List BuildConfigurationList = ['Release', 'Debug']
@@ -7,7 +8,27 @@ class Constants {
   static final String GitLabProjectIdDefaultValue = '27'
   static final String GitLabTokenDefaultValue = 'pW-SiNxUqhEj29ES8Ghi'
 }
-class ConstantsServer {
+class PiplineScriptsParameters {
+  static List ServerList = ["\"" + ServerDetails.devServerName + "\"", "\"" + ServerDetails.recetteServerName + "\"", "\"" + ServerDetails.productionServerName + "\""]
+  static String ServerListScript = "return $ServerList"
+  static String ErrorScript = 'return ["ERROR"]'
+  static final String ScriptToDefineServerName = '''
+  def serverName = ''
+  if(Server.equals("''' + ServerDetails.devServerName + '''"))
+  {
+    serverName="''' + ServerDetails.devServer + '''"
+  }
+  else if(Server.equals("''' + ServerDetails.recetteServerName + '''"))
+  {
+    serverName="''' + ServerDetails.recetteServer + '''"
+  }else
+  {
+    serverName="''' + ServerDetails.productionServer + '''"
+  }
+  return "<input name='value' class='setting-input' value='${serverName}' type='text'>"
+  '''
+}
+class ServerDetails {
   static final String devServerName = 'Dev'
   static final String recetteServerName = 'Recette'
   static final String productionServerName = 'Production'
@@ -15,47 +36,21 @@ class ConstantsServer {
   static final String recetteServer = 'reclpo03.srr.fr'
   static final String productionServer = 'weblpo02.srr.fr'
 }
-class ConstantsFolders {
+class FolderNames {
   static final String devBatchsFolderName = 'Batch'
   static final String recetteBatchsFolderName = 'Batchs'
   static final String productionBatchsFolderName = 'Batchs'
-
   static final String devWebsitesFolderName = 'Websites'
   static final String recetteWebsitesFolderName = 'Websites'
   static final String productionWebsitesFolderName = 'Websites'
-
   static final String devServicesFolderName = 'services'
   static final String recetteServicesFolderName = 'Services'
   static final String productionServicesFolderName = 'Services'
-
   static final String archiveFolderName = 'Archive'
   static final String hardDisk = 'd$'
 }
-class ConstantsScripts {
+// End Constants Region
 
-  String buildScript(List values) {
-    return "return $values"
-  }
-
-  static final List ServerList = ["\"" + ConstantsServer.devServerName + "\"", "\"" + ConstantsServer.recetteServerName + "\"", "\"" + ConstantsServer.productionServerName + "\""]
-  static String ServerListScript = "return $ServerList"
-
-  static final String ScriptToDefineServerName = '''
-  def serverName = ''
-  if(Server.equals("''' + ConstantsServer.devServerName + '''"))
-  {
-    serverName="''' + ConstantsServer.devServer + '''"
-  }
-  else if(Server.equals("''' + ConstantsServer.recetteServerName + '''"))
-  {
-    serverName="''' + ConstantsServer.recetteServer + '''"
-  }else
-  {
-    serverName="''' + ConstantsServer.productionServer + '''"
-  }
-  return "<input name='value' class='setting-input' value='${serverName}' type='text'>"
-  '''
-}
 class BuildDetails {
   String Project
   //serverName
@@ -65,10 +60,10 @@ class BuildDetails {
   String ArchiveDate
   String FileName = "${this.Project}-${this.ArchiveDate}"
   //archiveDirectory
-  String ArchiveFolder = "\\\\${this.ServerURL}\\${ConstantsFolders.archiveFolderName}\\${this.FileName}"
+  String ArchiveFolder = "\\\\${this.ServerURL}\\${FolderNames.archiveFolderName}\\${this.FileName}"
   String BatchsDeployPath = "\\\\${this.ServerURL}\\${this.HardDisk}\\${this.DeployFolder}\\${this.Project}"
   //specialCharacter
-  String HardDisk = ConstantsFolders.hardDisk
+  String HardDisk = FolderNames.hardDisk
   //DEPLOY_ENV
   String BuildConfiguration
   String BuildPlatforme
@@ -97,31 +92,31 @@ class BuildDetails {
   }
   @NonCPS
   def getDeployFolder(Server, ProjectType) {
-    if (ConstantsServer.devServerName == Server) {
+    if (ServerDetails.devServerName == Server) {
       if ('Batch' == ProjectType) {
-        return ConstantsFolders.devBatchsFolderName
+        return FolderNames.devBatchsFolderName
       }
       if ('Service' == ProjectType) {
-        return ConstantsFolders.devServicesFolderName
+        return FolderNames.devServicesFolderName
       }
-      return ConstantsFolders.devWebsitesFolderName
+      return FolderNames.devWebsitesFolderName
     }
-    else if (ConstantsServer.recetteServerName == Server) {
+    else if (ServerDetails.recetteServerName == Server) {
       if ('Batch' == ProjectType) {
-        return ConstantsFolders.recetteBatchsFolderName
+        return FolderNames.recetteBatchsFolderName
       }
       if ('Service' == ProjectType) {
-        return ConstantsFolders.recetteServicesFolderName
+        return FolderNames.recetteServicesFolderName
       }
-      return ConstantsFolders.recetteWebsitesFolderName
+      return FolderNames.recetteWebsitesFolderName
     }
       if ('Batch' == ProjectType) {
-        return ConstantsFolders.productionBatchsFolderName
+        return FolderNames.productionBatchsFolderName
       }
       if ('Service' == ProjectType) {
-        return ConstantsFolders.productionServicesFolderName
+        return FolderNames.productionServicesFolderName
       }
-    return ConstantsFolders.productionWebsitesFolderName
+    return FolderNames.productionWebsitesFolderName
   }
 }
 
@@ -129,12 +124,12 @@ properties([
 parameters([[
 $class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', name: 'Server', description: 'Serveur', script: [
 $class: 'GroovyScript', fallbackScript: [
-script: 'return ["ERROR"]'], script: [
-script: ConstantsScripts.ServerListScript]]], [
+script: PiplineScriptsParameters.ErrorScript], script: [
+script: PiplineScriptsParameters.ServerListScript]]], [
 $class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', omitValueField: false, referencedParameters: 'Server', name: 'ServerURL', script: [
 $class: 'GroovyScript', fallbackScript: [
-classpath: [], sandbox: true, script: 'return[\'Could not get any info\']'], script: [
-classpath: [], sandbox: false, script: ConstantsScripts.ScriptToDefineServerName]]]])])
+classpath: [], sandbox: true, script: PiplineScriptsParameters.ErrorScript], script: [
+classpath: [], sandbox: false, script: PiplineScriptsParameters.ScriptToDefineServerName]]]])])
 
 def initializeBuildDetails() {
   return new BuildDetails(params.Project, params.ServerURL, params.Server, params.BuildConfiguration, params.BuildPlatforme, currentBuild.number.toString(), params.GitLabToken, params.GitLabProjectId, params.ProjectType)
@@ -143,12 +138,12 @@ envbuildDetailstest = initializeBuildDetails()
 pipeline {
   agent any
   parameters {
-    string(name: 'Project', defaultValue: Constants.Project)
-    choice(name: 'ProjectType', choices: Constants.ProjectTypeList, description: 'Type de projet')
-    choice(name: 'BuildConfiguration', choices: Constants.BuildConfigurationList, description: 'Configuration de la solution')
-    choice(name: 'BuildPlatforme', choices: Constants.BuildPlateformeList, description: 'Plateforme de la solution')
-    string(name: 'GitLabProjectId', defaultValue: Constants.GitLabProjectIdDefaultValue)
-    string(name: 'GitLabToken', defaultValue: Constants.GitLabTokenDefaultValue)
+    string(name: 'Project', defaultValue: PiplineParameters.Project)
+    choice(name: 'ProjectType', choices: PiplineParameters.ProjectTypeList, description: 'Type de projet')
+    choice(name: 'BuildConfiguration', choices: PiplineParameters.BuildConfigurationList, description: 'Configuration de la solution')
+    choice(name: 'BuildPlatforme', choices: PiplineParameters.BuildPlateformeList, description: 'Plateforme de la solution')
+    string(name: 'GitLabProjectId', defaultValue: PiplineParameters.GitLabProjectIdDefaultValue)
+    string(name: 'GitLabToken', defaultValue: PiplineParameters.GitLabTokenDefaultValue)
   }
   stages {
     stage('Build') {
